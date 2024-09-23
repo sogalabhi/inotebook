125c581f9b7ef09531be24a67c097e7438c01589
sogalabhi <abhijithsogal@gmail.com>
2024-09-23 17:49:43 +0530

    Implemented middleware to fetch user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const User = require("../models/User")
-const router = express.Router();
+var jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
+
+const router = express.Router();
 
 router.post('/createuser', [
     body('name').isLength({ min: 3 }),
@@ -14,6 +15,7 @@ router.post('/createuser', [
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
+    
     let user = await User.findOne({ email: req.body.email });
     if (user) {
         return res.status(400).json({ error: "Duplicate email" });
@@ -34,9 +36,39 @@ router.post('/createuser', [
             }
             var token = jwt.sign(data, 'shhhhh');
 
-            res.json({token})
+            res.json({ token })
         });
     });
+
+})
+router.post('/login', [
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+    if (!user) {
+        return res.status(400).json({ error: "Invalid credentials" });
+    }
+    
+    const match = await bcrypt.compare(password, user.password);
+    if(!match){
+        return res.status(400).json({ error: "Invalid credentials" });
+    }
+    var data = {
+        user: { id: user.id }
+    }
+    var token = jwt.sign(data, 'shhhhh');
+
+    res.json({ token })
+    } catch (error) {
+        return res.status(400).json({ error: "internal server error" });
+    }
 
 })
 
